2142e2a349358a513af7e7e441e3946383d8a6d1
Fredl <fredl99@users.noreply.github.com>
2015-03-13 12:50:28 +0100

    Mostly some clean-up: 	Code shrinked by using a function for i10n/i18l translations 	More elaborative feedback after successful upload of a file

--- plugin.php
+++ plugin.php
@@ -3,15 +3,14 @@
 Plugin Name: Upload & Shorten
 Plugin URI: https://github.com/fredl99/YOURLS-Upload-and-Shorten
 Description: Upload a file and create a short-YOURL for it in one step.
-Based on: "Share Files" by Matt Temple
-Forked from: http://www.mattytemple.com/projects/yourls-share-files
-Version: 1.2
+Version: 1.3/testing
 Author: Fredl
 Author URI: https://github.com/fredl99
 */
 
 // No direct call
 if( !defined( 'YOURLS_ABSPATH' ) ) die();
+
 
 // Register our plugin admin page
 yourls_add_action( 'plugins_loaded', 'my_upload_and_shorten_add_page' );
@@ -24,43 +23,40 @@ function my_upload_and_shorten_add_page() {
 	// parameters: page slug, page title, and function that will display the page itself
 }
 
+function my_say__($message) {
+	$my_upload_and_shorten_domain = 'upload-and-shorten';
+	return yourls_esc_html__($message, $my_upload_and_shorten_domain );
+	}
+	
 // Display admin page
 function my_upload_and_shorten_do_page() {
 	// Check if a form was submitted
 	if(isset($_POST['submit'])) $my_save_files_message = my_upload_and_shorten_save_files();
 
-	// prepare i18n messages
-	$my_upload_and_shorten_domain = 'upload-and-shorten';
-	$my_i18n = array();
-	$my_i18n[1] = yourls_esc_html__('Upload & Shorten', $my_upload_and_shorten_domain );
-	$my_i18n[2] = yourls_esc_html__('Here you can upload a file to your webserver and create a short-URL for it.', $my_upload_and_shorten_domain );
-	$my_i18n[3] = yourls_esc_html__('Select file to upload: ', $my_upload_and_shorten_domain );
-	$my_i18n[4] = yourls_esc_html__('Optional custom keyword: ', $my_upload_and_shorten_domain );
-	$my_i18n[5] = yourls_esc_html__('Randomize filename ', $my_upload_and_shorten_domain );
-	$my_i18n[6] = yourls_esc_attr__('  Go!  ', $my_upload_and_shorten_domain );
-
 	// input form
 	echo '
-	<h2> '.$my_i18n[1].' </h2>
-	<p> '.$my_i18n[2].' </p>
-	<form method="post" enctype="multipart/form-data"> <fieldset>
-	<p><label for="file_upload">' .$my_i18n[3]. '</label> <input type="file" id="file_upload" name="file_upload" /></p>
-	<p><label for="custom_keyword">' .$my_i18n[4]. '</label> <input type="text" id="custom_keyword" name="custom_keyword" /></p>
-	<p><input type="checkbox" id="randomize_filename" name="randomize_filename" checked="checked" /><label for="randomize_filename">' .$my_i18n[5]. '</label> <small>(mypicture.jpg -> http://domain.tld/9a3e97434689.jpg)</small></p>
-	<p><input type="submit" name="submit" value="' .$my_i18n[6]. '" /></p>
+	<h2>' . my_say__("Upload & Shorten") . '</h2>
+	<p>' . my_say__("Here you can upload a file to your webserver and create a short-URL for it.") . '</p>
+
+	<form method="post" enctype="multipart/form-data"> 
+	<fieldset>
+	<p><label for="file_upload">' . my_say__("Select file to upload: ") . '</label> <input type="file" id="file_upload" name="file_upload" /></p>
+	<p><label for="custom_keyword">' . my_say__("Optional custom keyword: ") . '</label> <input type="text" id="custom_keyword" name="custom_keyword" /></p>
+	<p><input type="checkbox" id="randomize_filename" name="randomize_filename" checked="checked" /><label for="randomize_filename">' . my_say__("Randomize filename ") . '</label> <small>(mypicture.jpg -> http://domain.tld/9a3e97434689.jpg)</small></p>
+	<p><input type="submit" name="submit" value="' .my_say__("  Go!  "). '" /></p>
 	</fieldset></form>
-	<strong>' .$my_save_files_message. '</strong>
-	<div id="footer">This plugin is based on the plugin <a href="http://www.mattytemple.com/projects/yourls-share-files" target="_blank">"Share Files" by Matt Temple </a> with a few enhancements. The more you use it the more you\'ll like it! </div>';
+
+	<p><strong>' .$my_save_files_message. '</strong></p>
+	
+	<div id="footer">' . my_say__("This plugin is based on the plugin ") . '<a href="http://www.mattytemple.com/projects/yourls-share-files" target="_blank">"Share Files" by Matt Temple </a>' . my_say__(" with a few enhancements. The more you use it the more you'll like it!") . '</div>';
 	}
 
 // Update option in database
 function my_upload_and_shorten_save_files() {
-	// once again for translations:
-	$my_upload_and_shorten_domain = 'upload-and-shorten';
 
 	// did the user select any file?
 	if ($_FILES['file_upload']['error'] == UPLOAD_ERR_NO_FILE) {
-		return yourls_esc_html__('You need to select a file to upload.', $my_upload_and_shorten_domain);
+		return my_say__('You need to select a file to upload.');
 	}
 	
 	// yes!
@@ -107,9 +103,11 @@ function my_upload_and_shorten_save_files() {
 		
 		$my_short_url = yourls_add_new_link($my_url.$my_final_file_name, $my_custom_keyword, $my_final_file_name);
 		
-		return yourls_esc_html__('Upload finished. This is your short-URL: ', $my_upload_and_shorten_domain) . '<a href="' . $my_short_url['shorturl'] . '" target="_blank">' . $my_short_url['shorturl'] . '</a></strong>';
+		return 	my_say__('Upload finished. These are the links to your file ') . '"' .$my_filename.'.'.$my_extension . '":<br />' . 
+			my_say__('Direct URL: '). '<a href="' .$my_url.$my_final_file_name. '" target="_blank">' .$my_url.$my_final_file_name. '</a><br />' .
+			my_say__('Short URL: ') . '<a href="' .$my_short_url['shorturl'] . '" target="_blank">' . $my_short_url['shorturl'] . '</a>';
 	}
 	else {
-		return yourls_esc_html__('Upload failed! Something went wrong, sorry! :(', $my_upload_and_shorten_domain);
+		return my_say__('Upload failed! Something went wrong, sorry! :(');
 	}
 }
