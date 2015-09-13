846b0eae1cd9046f5e4994a094c0cbb8ca642e3c
Fredl <fredl99@users.noreply.github.com>
2015-09-13 14:52:45 +0200

    New feature: * Set a custom title which is only shown in the admin's list of short-URLs. Handy to give a description of the uploaded file. Set to the original filename as  default, so this information is kept even when the filename is randomized during the upload. New option: * Drop the file's extension in order to obfuscate the filetype.

--- plugin.php
+++ plugin.php
@@ -35,20 +35,36 @@ function my_upload_and_shorten_do_page() {
 
 	// input form
 	echo '
-	<h2>' . my_say__("Upload & Shorten") . '</h2>
-	<p>' . my_say__("Here you can upload a file to your webserver and create a short-URL for it.") . '</p>
+	<h2>'.my_say__("Upload & Shorten").'</h2>
+	<p>'.my_say__("Here you can upload a file to your webserver and create a short-URL for it.").'</p>
 
 	<form method="post" enctype="multipart/form-data"> 
 	<fieldset>
-	<p><label for="file_upload">' . my_say__("Select file to upload: ") . '</label> <input type="file" id="file_upload" name="file_upload" /></p>
-	<p><label for="custom_keyword">' . my_say__("Optional custom keyword: ") . '</label> <input type="text" id="custom_keyword" name="custom_keyword" /></p>
-	<p><input type="checkbox" id="randomize_filename" name="randomize_filename" checked="checked" /><label for="randomize_filename">' . my_say__("Randomize filename ") . '</label> <small>(mypicture.jpg -> http://domain.tld/9a3e97434689.jpg)</small></p>
-	<p><input type="submit" name="submit" value="' .my_say__("  Go!  "). '" /></p>
+
+	<p><label for="file_upload">'.my_say__("Select file to upload: ").'</label> <input type="file" id="file_upload" name="file_upload" /></p>
+	
+	<p><label for="custom_keyword">'.my_say__("Optional custom keyword: ").'</label> <input type="text" id="custom_keyword" name="custom_keyword" /></p>
+	
+	<p><label for="custom_title">'.my_say__("Optional custom title: ").'</label> <input type="text" id="custom_title" name="custom_title" value="'.$my_filename.'" /></p>
+	
+	<p><input type="checkbox" id="randomize_filename" name="randomize_filename" checked="checked" /><label for="randomize_filename">'.my_say__("Randomize filename ").'</label> <small>(mypicture.jpg -> http://domain.tld/9a3e97434689.jpg)</small></p>
+	
+	<p><input type="checkbox" id="drop_extension" name="drop_extension" /><label for="drop_extension">'.my_say__("Drop the filename's extension ").'</label> <small>(mypicture.jpg -> http://domain.tld/mypicture or http://domain.tld/9a3e97434689)</small></p>
+	
+	<p><input type="submit" name="submit" value="'.my_say__("  Go!  ").'" /></p>
+	
 	</fieldset></form>
 
-	<p><strong>' .$my_save_files_message. '</strong></p>
+	<p><strong>'.$my_save_files_message.'</strong></p>
 	
-	<div id="footer">' . my_say__("This plugin is based on the plugin ") . '<a href="http://www.mattytemple.com/projects/yourls-share-files" target="_blank">"Share Files" by Matt Temple </a>' . my_say__(" with a few enhancements. The more you use it the more you'll like it!") . '</div>';
+	<div id="footer">'	.my_say__("This plugin is hosted at ")
+				.'<a href="https://github.com/fredl99/YOURLS-Upload-and-Shorten" target="_blank">GitHub</a>'
+				.my_say__("If you like it, remember someone spends his time on it. ")
+				.'<a href="https://fredls.net/donate" target="_blank">'
+				.my_say__("Click here")
+				.'</a>'
+				.my_say__(" to buy him a coffee and motivate him for further improvements. ;-)")
+				.'</div>'
 	}
 
 // Update option in database
@@ -66,8 +82,14 @@ function my_upload_and_shorten_save_files() {
 	$my_uploaddir = SHARE_DIR;	// has to be defined in user/config.php like this: 
 					// define( 'SHARE_DIR', '/full/path/to/httpd/directory/' );	
 
-	$my_extension = pathinfo($_FILES['file_upload']['name'], PATHINFO_EXTENSION);
 	$my_filename = pathinfo($_FILES['file_upload']['name'], PATHINFO_FILENAME);
+	$my_extension = '';
+        if(! isset($_POST['drop_extension'])) {
+	// If this option is checked then leave away the filename's extension to obfuscate the filetype. 
+	// Beware: Some webservers won't send an appropriate HTTP-Header then!
+	// Negative logic: only set the extension, if the option is NOT selected:
+		$my_extension = '.' . pathinfo($_FILES['file_upload']['name'], PATHINFO_EXTENSION);
+	}
 
 	if(isset($_POST['randomize_filename'])) {
 		// make up a random name for the uploaded file
@@ -86,22 +108,30 @@ function my_upload_and_shorten_save_files() {
 
 	// avoid duplicate filenames
 	$my_count = 2;
-	$my_path = $my_uploaddir.$my_safe_filename.'.'.$my_extension;
-	$my_final_file_name = $my_safe_filename.'.'.$my_extension;
+	$my_path = $my_uploaddir.$my_safe_filename.$my_extension;
+	$my_final_file_name = $my_safe_filename.$my_extension;
 	while(file_exists($my_path)) {
-		$my_path = $my_uploaddir.$my_safe_filename.'-'.$my_count.'.'.$my_extension;
-		$my_final_file_name = $my_safe_filename.'-'.$my_count.'.'.$my_extension;
+		$my_path = $my_uploaddir.$my_safe_filename.'-'.$my_count.$my_extension;
+		$my_final_file_name = $my_safe_filename.'-'.$my_count.$my_extension;
 		$my_count++;	
 	}
-	
+
 	// move the file from /tmp/ to destination and initiate link creation
 	if(move_uploaded_file($_FILES['file_upload']['tmp_name'], $my_path)) {
+		// On success:
+		// obey custom keyword, if given:
 		$my_custom_keyword = NULL;
 		if(isset($_POST['custom_keyword']) && $_POST['custom_keyword'] != '') {
 			$my_custom_keyword = $_POST['custom_keyword'];
 		}
+		// change custom title, if given. Default is original filename, but if empty then use 'No title':
+		$my_custom_title = 'No title';
+		if(isset($_POST['custom_title']) && $_POST['custom_title'] != '') {
+			$my_custom_title = $_POST['custom_title'];
+		}
 		
-		$my_short_url = yourls_add_new_link($my_url.$my_final_file_name, $my_custom_keyword, $my_final_file_name);
+		// let YOURLS create the link:
+		$my_short_url = yourls_add_new_link($my_url.$my_final_file_name, $my_custom_keyword, $my_custom_title);
 		
 		return 	my_say__('Upload finished. These are the links to your file ') . '"' .$my_filename.'.'.$my_extension . '":<br />' . 
 			my_say__('Direct URL: '). '<a href="' .$my_url.$my_final_file_name. '" target="_blank">' .$my_url.$my_final_file_name. '</a><br />' .
